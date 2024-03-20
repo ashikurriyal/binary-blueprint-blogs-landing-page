92a8ccdc57d9dcaf9aa51b3111641c1a6fd826ba
ashikurriyal <riyal.ashikur@gmail.com>
2024-03-20 22:11:35 +0600

    Updated the structure of the blogs with author image, name, posted date, title, mins read section and hashtags

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,10 +1,38 @@
 import PropTypes from 'prop-types';
 
-const Blog = ({blog}) => {
-    console.log(blog)
+const Blog = ({ blog }) => {
+    const { title, cover, reading_time, author, author_img, posted_date, hashtags } = blog;
     return (
-        <div>
-            
+        <div className='space-y-3 flex flex-col mb-5'>
+
+            {/* coverImage */}
+            <img className='rounded-2xl' src={cover} alt="" />
+
+            {/* author and name div */}
+            <div className='flex justify-between items-center'>
+                <div className='flex gap-6'>
+                    <img className='w-16 rounded-full' src={author_img} alt="" />
+                    <div className='flex flex-col items-start'>
+                        <h4 className='font-exo font-bold text-black text-2xl'>{author}</h4>
+                        <p>{posted_date}</p>
+                    </div>
+                </div>
+                {/* mins read div */}
+                <div>
+                    <span>{reading_time} min read</span>
+                </div>
+            </div>
+
+            {/* Title */}
+
+            <div className="font-exo text-4xl font-bold text-left">{title}</div>
+
+            {/* hashtags */}
+            <div className='text-left flex gap-4'>
+                {
+                    hashtags.map((hash,idx) => <span key={idx} ><a  href="">#{hash}</a></span>)
+                }
+            </div>
         </div>
     );
 };
--- src/Components/Blogs/Blogs.jsx
+++ src/Components/Blogs/Blogs.jsx
@@ -1,18 +1,25 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import Blog from "../Blog/Blog";
 
 const Blogs = () => {
 
     const [blogsDataLoad, setBlogsDataLoad] = useState([]);
 
-    useEffect( () =>{
+    useEffect(() => {
         fetch('data.JSON')
-        .then(res => res.json())
-        .then(data => setBlogsDataLoad(data))
-    },[])
+            .then(res => res.json())
+            .then(data => setBlogsDataLoad(data))
+    }, [])
     return (
         <div className="md:w-2/3">
             <h2>Blogs: {blogsDataLoad.length}</h2>
+            {
+                blogsDataLoad.map(blog => <Blog
+                    key={blogsDataLoad.id}
+                    blog={blog}
+                ></Blog>)
+            }
         </div>
     );
 };
